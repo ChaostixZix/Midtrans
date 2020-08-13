0c91eb30ba51853da4f7443b9fee73f54cfc3044
Bintang Putra <chaostix@Bintangs-iMac.local>
2020-08-13 22:20:10 +0700

    s

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::any('handler', 'DonationController@submitDonation')->name('handler');
 
 
-Route::post('donation/store', 'DonationController@submitDonation')->name('donation.store');
-Route::post('finish', 'DonationController@handler')->name('donation.finish');
+Route::post('donation/store', 'MidtransController@submitDonation')->name('donation.store');
+Route::post('finish', 'MidtransController@handler')->name('donation.finish');
 Route::get('getsnap/{amount?}', 'MidtransController@getSnap')->name('donation.getsnap');
-Route::post('notification/handler', 'DonationController@notificationHandler')->name('notification.handler');
+Route::post('notification/handler', 'MidtransController@handler')->name('notification.handler');
 
